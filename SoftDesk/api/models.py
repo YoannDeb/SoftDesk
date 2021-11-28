acc86b3d5c4560c092334745faf39979f626307a
Yoann <yoann.deblois@gmail.com>
2021-11-28 22:45:47 +0100

    refactor: renamed models to singular

--- SoftDesk/api/models.py
+++ SoftDesk/api/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     pass
 
 
-class Contributors(models.Model):
+class Contributor(models.Model):
 
     AUTHOR = 'AU'
     CONTRIBUTOR = 'CO'
@@ -18,13 +18,13 @@ class Contributors(models.Model):
     ]
 
     user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='contributors')
-    project_id = models.ForeignKey('api.Projects', on_delete=models.CASCADE, related_name='contributors')
+    project_id = models.ForeignKey('api.Project', on_delete=models.CASCADE, related_name='contributors')
 
     permission = models.CharField(max_length=2, choices=PERMISSION_CHOICES)  # Unique author? a priori yes
     role = models.CharField(max_length=300)
 
 
-class Projects(models.Model):
+class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     type = models.CharField(max_length=50)
@@ -32,21 +32,27 @@ class Projects(models.Model):
     # or
     # author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='Projects')
 
+    def __str__(self):
+        return self.title
 
-class Issues(models.Model):
+
+class Issue(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     tag = models.CharField(max_length=50)
     priority = models.CharField(max_length=50)
-    project_id = models.ForeignKey('api.Projects', on_delete=models.CASCADE, related_name='issues')
+    project_id = models.ForeignKey('api.Project', on_delete=models.CASCADE, related_name='issues')
     status = models.CharField(max_length=50)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_issues')
     assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assigned_issues')
     created_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
 
-class Comments(models.Model):
+
+class Comment(models.Model):
     description = models.CharField(max_length=300)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
-    issue_id = models.ForeignKey('api.Issues', on_delete=models.CASCADE, related_name='comments')
+    issue_id = models.ForeignKey('api.Issue', on_delete=models.CASCADE, related_name='comments')
     created_time = models.DateTimeField(auto_now_add=True)
