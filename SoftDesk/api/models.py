b2756817b6fb0b4b52eea2aab652463770b234b8
Yoann <yoann.deblois@gmail.com>
2021-11-28 17:34:07 +0100

    Initial commit

---
+++ SoftDesk/api/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
+
+class User(AbstractUser):
+    pass
+
+
+class Contributors(models.Model):
+
+    AUTHOR = 'AU'
+    CONTRIBUTOR = 'CO'
+
+    PERMISSION_CHOICES = [
+        (AUTHOR, 'Author'),
+        (CONTRIBUTOR, 'Contributor'),
+    ]
+
+    user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='contributors')
+    project_id = models.ForeignKey('api.Projects', on_delete=models.CASCADE, related_name='contributors')
+
+    permission = models.CharField(max_length=2, choices=PERMISSION_CHOICES)  # Unique author? a priori yes
+    role = models.CharField(max_length=300)
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    type = models.CharField(max_length=50)
+    # Use something like project.Contributors.objects.filter(project_id=pk).filter(permission=AUTHOR)
+    # or
+    # author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='Projects')
+
+
+class Issues(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    tag = models.CharField(max_length=50)
+    priority = models.CharField(max_length=50)
+    project_id = models.ForeignKey('api.Projects', on_delete=models.CASCADE, related_name='issues')
+    status = models.CharField(max_length=50)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_issues')
+    assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assigned_issues')
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    description = models.CharField(max_length=300)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    issue_id = models.ForeignKey('api.Issues', on_delete=models.CASCADE, related_name='comments')
+    created_time = models.DateTimeField(auto_now_add=True)
