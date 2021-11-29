ad1c3124ce8c59e47b6da2c2379831ff22933746
Yoann <yoann.deblois@gmail.com>
2021-11-29 17:33:03 +0100

    func: projects, Comments and issues in admin

--- SoftDesk/api/admin.py
+++ SoftDesk/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Project, Comment, Issue
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Project)
+admin.site.register(Comment)
+admin.site.register(Issue)
