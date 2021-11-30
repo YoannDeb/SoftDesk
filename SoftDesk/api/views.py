bec0e951b0ca098cc90566a8fd47d486b69ea14d
Yoann <yoann.deblois@gmail.com>
2021-11-30 22:39:16 +0100

    func: prototypes of issues and comments model views

--- SoftDesk/api/views.py
+++ SoftDesk/api/views.py
@@ -4,15 +4,37 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from .models import Project, Contributor, Issue, Comment
-from .serializers import ProjectListSerializer, ProjectDetailSerializer
+# from .serializers import ProjectListSerializer, ProjectDetailSerializer
+from .serializers import ProjectSerializer, CommentSerializer, IssueSerializer
 
+
+# class ProjectViewSet(ModelViewSet):
+#     serializer_class = ProjectListSerializer
+#     detail_serializer_class = ProjectDetailSerializer
+#     queryset = Project.objects.all()  #TODO to change in something like Project.objects.filter(contributors.user_id=settings.AUTH_USER_MODEL) when auth is done
+#
+#     def get_serializer_class(self):
+#         if self.action == 'retrieve':
+#             return self.detail_serializer_class
+#         return super().get_serializer_class()
 
 class ProjectViewSet(ModelViewSet):
-    serializer_class = ProjectListSerializer
-    detail_serializer_class = ProjectDetailSerializer
+    serializer_class = ProjectSerializer
     queryset = Project.objects.all()  #TODO to change in something like Project.objects.filter(contributors.user_id=settings.AUTH_USER_MODEL) when auth is done
 
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return super().get_serializer_class()
+
+class IssueViewSet(ModelViewSet):
+    serializer_class = IssueSerializer
+
+    def get_queryset(self):
+        return Issue.objects.filter(project_id=self.kwargs['project_id'])
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.filter(issue_id=self.kwargs['issue_id'])
+
+
+
