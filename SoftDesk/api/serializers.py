16a9793f0f9b1d824c036ec25167477bf5d949bc
Yoann <yoann.deblois@gmail.com>
2021-12-01 11:34:10 +0100

    func: creation functional yet still WIP

--- SoftDesk/api/serializers.py
+++ SoftDesk/api/serializers.py
@@ -12,41 +12,16 @@ class ContributorSerializer(ModelSerializer):
         fields = ['id', 'user_id', 'user_username', 'project_id', 'project_title', 'permission', 'role']
 
 
-# class ContributorDetailSerializer(ModelSerializer):
-#     user_username = CharField(read_only=True, source='user.username')
-#     project_title = CharField(read_only=True, source='project.title')
-#
-#     class Meta:
-#         model = Contributor
-#         fields = ['id', 'user_id', 'user_username', 'project_id', 'project_title', 'permission', 'role']
-
-
 class CommentSerializer(ModelSerializer):
-
     class Meta:
         model = Comment
         fields = ['id', 'description', 'author_user_id', 'issue_id', 'created_time']
 
 
-# class CommentDetailSerializer(ModelSerializer):
-#
-#     class Meta:
-#         model = Comment
-#         fields = ['id', 'description', 'author_user_id', 'issue_id', 'created_time']
-
-
 class IssueSerializer(ModelSerializer):
-
     class Meta:
         model = Issue
         fields = ['id', 'title', 'description', 'tag', 'priority', 'project_id', 'status', 'author_user_id', 'assignee_user_id', 'created_time']
-
-
-# class IssueDetailSerializer(ModelSerializer):
-#
-#     class Meta:
-#         model = Issue
-#         fields = ['id', 'title', 'description', 'tag', 'priority', 'project_id', 'status', 'author_user_id', 'assignee_user_id', 'created_time']
 
 
 class ProjectSerializer(ModelSerializer):
@@ -58,49 +33,16 @@ class ProjectSerializer(ModelSerializer):
         fields = ['id', 'title', 'description', 'type', 'author_user_id', 'contributors']
 
     def get_author_user_id(self, instance):
-        return instance.contributors.get(permission=Contributor.AUTHOR)['user_id']
-
-    # def get_contributors(self, instance):
-    #     queryset = instance.contributors.all()
-    #     serializer = ContributorSerializer(queryset, many=True)
-    #     return serializer.data
+        return instance.contributors.get(permission=Contributor.AUTHOR).user_id.pk
 
     def get_contributors(self, instance):
-        return instance.contributors.all()
+        queryset = instance.contributors.all()
+        serializer = ContributorSerializer(queryset, many=True)
+        return serializer.data
 
-
-class ProjectDetailSerializer(ModelSerializer):
-
-    contributors = SerializerMethodField()
-    author_user_id = SerializerMethodField()
-
-    class Meta:
-        model = Project
-        fields = ['id', 'title', 'description', 'type', 'author_user_id', 'contributors']
-
-    def get_author_user_id(self, instance):
-        return instance.contributors.get(permission=Contributor.AUTHOR)['user_id']
-
-
-# class ProjectUsersListSerializer(ModelSerializer):
-#     pass
-#
-#
-# class ProjectUsersDetailSerializer(ModelSerializer):
-#     pass
-#
-#
-# class ProjectIssuesListSerializer(ModelSerializer):
-#     pass
-#
-#
-# class ProjectIssuesDetailSerializer(ModelSerializer):
-#     pass
-#
-#
-# class ProjectIssueCommentsListSerializer(ModelSerializer):
-#     pass
-#
-#
-# class ProjectIssueCommentsDetailSerializer(ModelSerializer):
-#     pass
+    def create(self, validated_data):  # or data ?
+        project_instance = super().create(validated_data)
+        contributor = Contributor.objects.create(permission=Contributor.AUTHOR, project_id=project_instance,
+                                                 user_id=self.context['request'].user)
+        contributor.save()
+        return project_instance
