1ac4b8d586c72c4550181aa74b3920e0ef51d11b
Yoann <yoann.deblois@gmail.com>
2021-12-01 11:30:41 +0100

    func: added contributors to admin management

--- SoftDesk/api/admin.py
+++ SoftDesk/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Project, Comment, Issue
+from .models import User, Project, Comment, Issue, Contributor
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Project)
 admin.site.register(Comment)
 admin.site.register(Issue)
+admin.site.register(Contributor)
