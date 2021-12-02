f40e84d2071e5ec40aa785437fce48e97e329fb8
Yoann <yoann.deblois@gmail.com>
2021-12-02 10:49:16 +0100

    func: added simplejwt fin INSTALLED_APPS

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'api',
 ]
 
@@ -131,9 +132,11 @@ AUTH_USER_MODEL = 'api.User'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION8CLASSES':
+        ('rest_framework_simplejwt.authentication.JWTAuthentication',),
     #   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    #   'PAGE_SIZE': 6
+    #   'PAGE_SIZE': 6,
 }
 
 
