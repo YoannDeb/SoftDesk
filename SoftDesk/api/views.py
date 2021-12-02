c9cc0802ffe368e9befd5d077d055964d1a2c450
Yoann <yoann.deblois@gmail.com>
2021-12-02 10:51:05 +0100

    func: filtering of projects shown (only user contributions)

--- SoftDesk/api/views.py
+++ SoftDesk/api/views.py
@@ -1,26 +1,21 @@
 from django.conf import settings
 from rest_framework import viewsets
+from rest_framework import views
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from .models import Project, Contributor, Issue, Comment
-# from .serializers import ProjectListSerializer, ProjectDetailSerializer
 from .serializers import ProjectSerializer, CommentSerializer, IssueSerializer
 
+# class SignUpAPIView(views.APIView):
+#     permission_classes = (AllowAny,)
 
-# class ProjectViewSet(ModelViewSet):
-#     serializer_class = ProjectListSerializer
-#     detail_serializer_class = ProjectDetailSerializer
-#     queryset = Project.objects.all()  #TODO to change in something like Project.objects.filter(contributors.user_id=settings.AUTH_USER_MODEL) when auth is done
-#
-#     def get_serializer_class(self):
-#         if self.action == 'retrieve':
-#             return self.detail_serializer_class
-#         return super().get_serializer_class()
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
-    queryset = Project.objects.all()  #TODO to change in something like Project.objects.filter(contributors.user_id=settings.AUTH_USER_MODEL) when auth is done
+
+    def get_queryset(self):
+        return Project.objects.filter(contributors=self.request.user)
 
 
 class IssueViewSet(viewsets.ModelViewSet):
