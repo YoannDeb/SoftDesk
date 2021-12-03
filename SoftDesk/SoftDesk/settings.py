f0dd92ccb7ef391bc7a59deeef02bd807e04d979
Yoann <yoann.deblois@gmail.com>
2021-12-03 16:45:15 +0100

    func: simple-jwt token lifetime settings

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,4 +139,7 @@ REST_FRAMEWORK = {
     #   'PAGE_SIZE': 6,
 }
 
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
