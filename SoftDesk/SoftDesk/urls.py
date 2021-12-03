22dc719fa0e8f87398105ed01306e956e5077712
Yoann <yoann.deblois@gmail.com>
2021-12-03 19:08:48 +0100

    func: signup endpoint

--- SoftDesk/SoftDesk/urls.py
+++ SoftDesk/SoftDesk/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from rest_framework_nested import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from api.views import ProjectViewSet, IssueViewSet, CommentViewSet
+from api.views import ProjectViewSet, IssueViewSet, CommentViewSet, SignUpAPIView
 
 router = routers.SimpleRouter()
 router.register('projects', ProjectViewSet, basename='projects')
@@ -33,6 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/', SignUpAPIView.as_view()),
     path('', include(router.urls)),
     path('', include(project_router.urls)),
     path('', include(issue_router.urls)),
