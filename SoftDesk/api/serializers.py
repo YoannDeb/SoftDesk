adab332185e63ca63ad3aff154750c44c1b6e4c2
Yoann <yoann.deblois@gmail.com>
2021-12-05 18:43:44 +0100

    func: draft of alternative SignUp

--- SoftDesk/api/serializers.py
+++ SoftDesk/api/serializers.py
@@ -10,6 +10,15 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['email', 'first_name', 'last_name', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
+    # def create(self, validated_data):
+    #     user = User.objects.create_user(
+    #         first_name=validated_data['first_name'],
+    #         last_name=validated_data['last_name'],
+    #         email=validated_data['email'],
+    #         password=validated_data['password'],
+    #     )
+    #     return user
+
 
 class ContributorSerializer(serializers.ModelSerializer):
     user_username = serializers.CharField(read_only=True, source='user.username')
