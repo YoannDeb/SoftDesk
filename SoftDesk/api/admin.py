01e5dd24a9c21e759a00ed122881b97020b33d2a
Yoann <yoann.deblois@gmail.com>
2021-12-06 14:18:49 +0100

    func: WIP: had to remove admin user registering to migrate

--- SoftDesk/api/admin.py
+++ SoftDesk/api/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Project, Comment, Issue, Contributor
 
-admin.site.register(User, UserAdmin)
+
+# admin.site.register(User, UserAdmin)
 admin.site.register(Project)
 admin.site.register(Comment)
 admin.site.register(Issue)
