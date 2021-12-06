a4169fe7f83af1050990da2ec4528bdde69cd4bf
Yoann <yoann.deblois@gmail.com>
2021-12-06 14:17:58 +0100

    func: Email as username, WIP

--- SoftDesk/api/models.py
+++ SoftDesk/api/models.py
@@ -1,11 +1,57 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+# from django.utils import timezone
+from django.db import models, transaction
 from django.conf import settings
 
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        try:
+            with transaction.atomic():
+                user = self.model(email=email, **extra_fields)
+                user.set_password(password)
+                user.save(using=self._db)
+                return user
+        except:
+            raise
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    # date_joined = models.DateTimeField(default=timezone.now)
+
+    Objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.email}"
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
+
+# class User(AbstractUser):
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name} - {self.email}"
 
 
 class Contributor(models.Model):
