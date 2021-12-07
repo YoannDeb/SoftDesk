0a4e2fdb82aff8215bb71469a990bb2e089e6ffa
Yoann <yoann.deblois@gmail.com>
2021-12-07 15:31:58 +0100

    func: custom user verbose name is now user

--- SoftDesk/api/models.py
+++ SoftDesk/api/models.py
@@ -15,12 +15,10 @@ class CustomUserManager(BaseUserManager):
             return user
 
     def create_user(self, email, password=None, **extra_fields):
-        # extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
-        # extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self._create_user(email, password, **extra_fields)
 
@@ -29,9 +27,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True, verbose_name='email address')
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
-    # is_active = models.BooleanField(default=True)
-    # is_staff = models.BooleanField(default=False)
-    # date_joined = models.DateTimeField(default=timezone.now)
 
     Objects = CustomUserManager()
 
@@ -51,9 +46,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         # Simplest possible answer: All superusers are staff
         return self.is_superuser
 
-# class User(AbstractUser):
-#     def __str__(self):
-#         return f"{self.first_name} {self.last_name} - {self.email}"
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
 
 
 class Contributor(models.Model):
