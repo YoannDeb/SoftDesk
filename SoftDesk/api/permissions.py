33e3a341941ae1caf5068f138424b6c0f7c4e12f
Yoann <yoann.deblois@gmail.com>
2021-12-08 23:05:44 +0100

    func: WIP issues permissions ok

--- SoftDesk/api/permissions.py
+++ SoftDesk/api/permissions.py
@@ -3,19 +3,28 @@ from rest_framework.permissions import BasePermission
 
 class IsProjectContributor(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.project in request.user.projects)
+        project_pk = int(request.resolver_match.kwargs['project_pk'])
+        user_is_contributor_projects_pks = list(request.user.project_set.all().values_list('pk', flat=True))
+        return project_pk in user_is_contributor_projects_pks
 
 
 class IsProjectAuthor(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.project.author_user_id == request.user)
+        project_pk = int(request.resolver_match.kwargs['project_pk'])
+        user_is_author_project_pks = list(request.user.project_set.filter(author_user_id=request.user).values_list('pk', flat=True))
+        return project_pk in user_is_author_project_pks
 
 
 class IsCurrentUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user == request.context.user)
+        return request.user.pk == int(request.resolver_match.kwargs['user_pk'])
 
 
 class IsIssueAuthor(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user == request.issue.author_user_id)
+        return request.user.pk in list(request.user.issues.all().values_list('pk', flat=True))
+
+
+class IsCommentAuthor(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.pk in list(request.user.comments.all().values_list('pk', flat=True))
