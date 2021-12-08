178981ea762238e87e9664d045bb2012c9079a44
Yoann <yoann.deblois@gmail.com>
2021-12-08 16:51:24 +0100

    func: added Projects view permissions

--- SoftDesk/api/views.py
+++ SoftDesk/api/views.py
@@ -28,6 +28,7 @@ class SignUpAPIView(views.APIView):
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Project.objects.filter(contributors=self.request.user)
