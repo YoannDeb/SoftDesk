5e5579c447466b88589d911d5f8e27e8ce51cd9e
Yoann <yoann.deblois@gmail.com>
2021-12-08 20:48:42 +0100

    func: updated permissions

--- SoftDesk/api/permissions.py
+++ SoftDesk/api/permissions.py
@@ -2,10 +2,20 @@ from rest_framework.permissions import BasePermission
 
 
 class IsProjectContributor(BasePermission):
-    def has_permissions(self, request, view):
-        return bool(request.user and request.user.is_authhenticated and request.project in request.user.projects)
+    def has_permission(self, request, view):
+        return bool(request.user and request.project in request.user.projects)
 
 
 class IsProjectAuthor(BasePermission):
-    def has_permissions(self, request, view):
-        return bool(request.user and request.user.is_authhenticated and request.project.author_user_id == request.user)
+    def has_permission(self, request, view):
+        return bool(request.user and request.project.author_user_id == request.user)
+
+
+class IsCurrentUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user == request.context.user)
+
+
+class IsIssueAuthor(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user == request.issue.author_user_id)
