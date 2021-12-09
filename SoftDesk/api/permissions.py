95f215c4255aa22467f31c9b33a8b1c125b34ce0
Yoann <yoann.deblois@gmail.com>
2021-12-09 19:09:49 +0100

    bugfix: retrieve method of project pk different depending on endpoint depth
    func: permission messages
    
    OK: IsProjectContributor and IsProjectAuthor

--- SoftDesk/api/permissions.py
+++ SoftDesk/api/permissions.py
@@ -2,29 +2,45 @@ from rest_framework.permissions import BasePermission
 
 
 class IsProjectContributor(BasePermission):
+    message = "Access forbidden: You are not contributor of the project"
+
     def has_permission(self, request, view):
-        project_pk = int(request.resolver_match.kwargs['project_pk'])
-        user_is_contributor_projects_pks = list(request.user.project_set.all().values_list('pk', flat=True))
-        return project_pk in user_is_contributor_projects_pks
+        try:
+            project_pk = int(request.resolver_match.kwargs['project_pk'])
+        except:
+            project_pk = int(request.resolver_match.kwargs['pk'])
+        projects_pks_user_is_contributor = list(request.user.project_set.all().values_list('pk', flat=True))
+        return project_pk in projects_pks_user_is_contributor
 
 
 class IsProjectAuthor(BasePermission):
+    message = "Access forbidden: You are not the author of the project"
+
     def has_permission(self, request, view):
-        project_pk = int(request.resolver_match.kwargs['project_pk'])
-        user_is_author_project_pks = list(request.user.project_set.filter(author_user_id=request.user).values_list('pk', flat=True))
-        return project_pk in user_is_author_project_pks
+        try:
+            project_pk = int(request.resolver_match.kwargs['project_pk'])
+        except:
+            project_pk = int(request.resolver_match.kwargs['pk'])
+        project = request.user.project_set.get(pk=project_pk)
+        return project.author_user_id == request.user.pk
 
 
 class IsCurrentUser(BasePermission):
+    message = "Access forbidden: You are not the user concerned"
+
     def has_permission(self, request, view):
-        return request.user.pk == int(request.resolver_match.kwargs['user_pk'])
+        return request.user.pk == int(request.resolver_match.kwargs['pk'])
 
 
 class IsIssueAuthor(BasePermission):
+    message = "Access forbidden: You are not the author of the issue"
+
     def has_permission(self, request, view):
         return request.user.pk in list(request.user.issues.all().values_list('pk', flat=True))
 
 
 class IsCommentAuthor(BasePermission):
+    message = "Access forbidden: You are not the author of the comment"
+
     def has_permission(self, request, view):
         return request.user.pk in list(request.user.comments.all().values_list('pk', flat=True))
