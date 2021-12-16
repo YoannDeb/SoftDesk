710d16f8fcb6109c856e8b9d5fdf2e9b1bc0256f
Yoann <yoann.deblois@gmail.com>
2021-12-16 06:59:42 +0100

    OWASP: removed debug mode

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9qgy1ufz#f)gn0+l0lsrd2h9vci6@5j58$+r=6b8hm1mrdoi$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
