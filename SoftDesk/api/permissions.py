f54ad96948294b6d9a364810df78f9c10f477c7e
Yoann <yoann.deblois@gmail.com>
2021-12-26 23:57:42 +0100

    func: If the object of which the permission is checked does not exist, it now returns an adapted message.

--- SoftDesk/api/permissions.py
+++ SoftDesk/api/permissions.py
@@ -1,4 +1,7 @@
 from rest_framework.permissions import BasePermission
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+from .models import Project, Contributor, Issue, Comment, CustomUser
 
 
 class IsProjectContributor(BasePermission):
@@ -7,8 +10,15 @@ class IsProjectContributor(BasePermission):
     def has_permission(self, request, view):
         try:
             project_pk = int(request.resolver_match.kwargs['project_pk'])
-        except:
+        except KeyError:
             project_pk = int(request.resolver_match.kwargs['pk'])
+
+        # Adapted message if project does not exist:
+        try:
+            Project.objects.get(pk=project_pk)
+        except ObjectDoesNotExist:
+            self.message = "Project does not exist"
+
         projects_of_which_user_is_contributor = list(request.user.project_set.all().values_list('pk', flat=True))
         return project_pk in projects_of_which_user_is_contributor
 
@@ -19,8 +29,15 @@ class IsProjectAuthor(BasePermission):
     def has_permission(self, request, view):
         try:
             project_pk = int(request.resolver_match.kwargs['project_pk'])
-        except:
+        except KeyError:
             project_pk = int(request.resolver_match.kwargs['pk'])
+
+        # Adapted message if project does not exist:
+        try:
+            Project.objects.get(pk=project_pk)
+        except ObjectDoesNotExist:
+            self.message = "Project does not exist"
+
         project = request.user.project_set.get(pk=project_pk)
         return project.author_user_id == request.user.pk
 
@@ -29,7 +46,15 @@ class IsCurrentUser(BasePermission):
     message = "Access forbidden: You are not the user concerned"
 
     def has_permission(self, request, view):
-        return request.user.pk == int(request.resolver_match.kwargs['pk'])
+        user_pk_in_url = int(request.resolver_match.kwargs['pk'])
+
+        # Adapted message if project does not exist:
+        try:
+            CustomUser.objects.get(pk=user_pk_in_url)
+        except ObjectDoesNotExist:
+            self.message = "User does not exist"
+
+        return request.user.pk == user_pk_in_url
 
 
 class IsIssueAuthor(BasePermission):
@@ -37,10 +62,16 @@ class IsIssueAuthor(BasePermission):
 
     def has_permission(self, request, view):
         try:
-            issue = int(request.resolver_match.kwargs['pk'])
+            issue_pk = int(request.resolver_match.kwargs['pk'])
             issues_of_which_user_is_the_author = list(request.user.created_issues.all().values_list('pk', flat=True))
-            return issue in issues_of_which_user_is_the_author
+            # Adapted message if issue does not exist:
+            Issue.objects.get(pk=issue_pk)
+            return issue_pk in issues_of_which_user_is_the_author
         except AttributeError:
+            return False
+        # Adapted message if issue does not exist:
+        except ObjectDoesNotExist:
+            self.message = "Issue does not exist"
             return False
 
 
@@ -49,8 +80,14 @@ class IsCommentAuthor(BasePermission):
 
     def has_permission(self, request, view):
         try:
-            comment = int(request.resolver_match.kwargs['pk'])
+            comment_pk = int(request.resolver_match.kwargs['pk'])
             comments_of_which_user_is_the_author = list(request.user.comments.all().values_list('pk', flat=True))
-            return comment in comments_of_which_user_is_the_author
+            # Adapted message if comment does not exist:
+            Comment.objects.get(pk=comment_pk)
+            return comment_pk in comments_of_which_user_is_the_author
         except AttributeError:
+            return False
+        # Adapted message if comment does not exist:
+        except ObjectDoesNotExist:
+            self.message = "Issue does not exist"
             return False
